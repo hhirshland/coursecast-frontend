802823c994c5b89fa70cffa9d21c631bec068a5c
Henry Hirshland <hhirshland@gmail.com>
2024-05-14 17:42:39 -0700

    adding get-current-group endpoint

---
+++ src/app/api/get-current-group/route.js
@@ -0,0 +1,28 @@
+//Supabase Variables
+const { createClient } = require("@supabase/supabase-js");
+const supabaseUrl = "https://ixbrsgekfioaizpwaogk.supabase.co";
+const supabaseKey =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Iml4YnJzZ2VrZmlvYWl6cHdhb2drIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTcwMTgxNjg4NywiZXhwIjoyMDE3MzkyODg3fQ.MTggLFvV5ZzR50oQCARPEcQs5SCzWvToLOj2ozI_XtY";
+
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+export async function GET(request) {
+  let { data: groups, error } = await supabase
+    .from("groups")
+    .select("*")
+    .order("group_id", { ascending: false })
+    .limit(1);
+  if (error) {
+    return Response.json({
+      message: "Error getting groups",
+    });
+  }
+  let groupId = groups[0].group_id;
+  if (groups[0].clip_1_generated == true) {
+    groupId = groupId + 1;
+  }
+
+  return Response.json({
+    groupId: groupId,
+  });
+}
